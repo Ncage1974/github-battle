bb72b735d1e9f60836175e024fa69bc3c1104111
brent <general@diaryofacoder.com>
2017-05-23 13:09:23 -0500

    added detail

--- app/components/Popular.jsx
+++ app/components/Popular.jsx
@@ -22,6 +22,36 @@ function SelectedLanguage(props) {
 
 }
 
+function RepoGrid(props) {
+    return (
+        <ul className='popular-list'>
+            {props.repos.map((repo, index) => {
+                return (
+                    <li key={repo.name} className='popular-item'>
+                        <div className='popular-rank'>#{index + 1}</div>
+                        <ul className='space-list-items'>
+                            <li>
+                                <img
+                                    className='avatar'
+                                    src={repo.owner.avatar_url}
+                                    alt={'Avatar for ' + repo.owner.login}
+                                />
+                            </li>
+                            <li><a href={repo.html_url}>{repo.name}</a></li>
+                            <li>@{repo.owner.login}</li>
+                            <li>{repo.stargazers_count} stars</li>
+                        </ul>
+                    </li>
+                )
+            })}
+        </ul>
+    )
+}
+
+RepoGrid.propTypes = {
+    repos: PropTypes.array.isRequired
+}
+
 SelectedLanguage.propTypes = {
     selectedLanguage: PropTypes.string.isRequired,
     onSelect: PropTypes.func.isRequired
@@ -46,7 +76,9 @@ class Popular extends React.Component {
 
         Api.fetchPopularRepos(lang)
             .then((repos) => {
-                console.log(repos);
+                this.setState({
+                    repos: repos
+                })
             });
     }
 
@@ -62,6 +94,9 @@ class Popular extends React.Component {
                     selectedLanguage={this.state.selectedLanguage}
                     onSelect={this.updateLanguage}
                 />
+                {!this.state.repos
+                    ? <p>LOADING</p>
+                    : <RepoGrid repos={this.state.repos} /> }                
             </div>
         )
 
--- app/utils/api.jsx
+++ app/utils/api.jsx
@@ -4,7 +4,7 @@ module.exports = {
     fetchPopularRepos: function(language)  {
         let encodedURI = window.encodeURI('https://api.github.com/search/repositories?q=stars:>1+language:' + language + 
         '&sort=stars&order=desc&type=Repositories');
-
+        console.log(encodedURI);
         return axios.get(encodedURI)
             .then((response) =>
             {
