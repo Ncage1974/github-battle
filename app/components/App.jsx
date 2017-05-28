ecf3dc773931e786e2ba76752f91227547b37e54
me <me@example.org>
2017-05-27 19:37:10 -0500

    added nav component

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,13 +1,19 @@
 var React = require('react');
 var Popular = require('./Popular.jsx');
+var ReactRouter = require('react-router-dom');
+var Router = ReactRouter.BrowserRouter;
+var Route = ReactRouter.Route;
 
 
 class App extends React.Component {
     render() {
         return (
-            <div className='container'>
-                <Popular />
-            </div>
+            <Router>
+                <div className='container'>
+                    <Popular />
+                </div>
+            </Router>
+
         );
     }
 }
