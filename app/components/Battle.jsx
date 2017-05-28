1b73481ada0b5a005269dba20ec938198365e48d
me <me@example.org>
2017-05-27 20:15:20 -0500

    added battle component

---
+++ app/components/Battle.jsx
@@ -0,0 +1,14 @@
+var React = require('react');
+
+
+class Battle extends React.Component {
+    Render() {
+        return (
+            <div>
+                Battle!!
+            </div>
+        )
+    }
+}
+
+module.exports = Battle;
