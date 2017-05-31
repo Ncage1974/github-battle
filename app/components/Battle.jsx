04a71f79deb3f1421a505ad5bf3a08de7073a247
brent <general@diaryofacoder.com>
2017-05-31 12:22:42 -0500

    starting implimenting the battle component

--- app/components/Battle.jsx
+++ app/components/Battle.jsx
@@ -1,11 +1,51 @@
 var React = require('react');
 
 
+
 class Battle extends React.Component {
+   
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            playerOneName: '',
+            playerTwoName: '',
+            playerOneImage: null,
+            playerTwoImage: null
+        }
+
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleSubmit() {
+        this.setState((id, username)=> {
+            var newstate = {};
+            newstate[id + 'Name'] = username;
+            newstate[id + 'Image'] = 'https://github.com/' + username + '.png?size=200';
+            return newstate;
+        })
+    }
     render() {
+        let playerOneName =  this.state.playerOneName;
+        let playerTwoName = this.state.playerTwoName;
+
         return (
             <div>
-                Battle!!
+                <div className='row'>
+                    {!playerOneName &&
+                    <PlayerInput 
+                        id='playerOne'
+                        label='Player One'
+                        onSubmit={this.handleSubmit}
+                     /> }
+
+                    {!playerTwoName &&
+                    <PlayerInput
+                        id='playerTwo'
+                        label='Player Two'
+                        onSubmit={this.handleSubmit}
+                     /> }
+                </div>
             </div>
         )
     }
