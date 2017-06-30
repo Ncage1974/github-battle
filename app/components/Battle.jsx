792c819e5b47ca18d22030665ef255316800c19e
brent <general@diaryofacoder.com>
2017-06-30 07:56:07 -0500

    Exported PlayerPreview to its own component so it could be shared.

--- app/components/Battle.jsx
+++ app/components/Battle.jsx
@@ -2,35 +2,7 @@ var React = require('react');
 var PropTypes = require('prop-types');
 //var Link = require('react-router').Link;
 import { Link } from 'react-router-dom';
-
-function PlayerPreview(props) {
-
-    return (
-
-        <div>
-            <div className='column'>
-                <img
-                    className='avatar'
-                    src={props.avatar}
-                    alt={`Avatar for ${props.username}`}
-                />
-                <h2 className='username'>@{props.username}</h2>
-            </div>
-            <button
-                className='reset'
-                onClick={props.onReset.bind(null, props.id)}>
-                Reset
-            </button>
-        </div>
-    )
-}
-
-PlayerPreview.propTypes = {
-    avatar: PropTypes.string.isRequired,
-    username: PropTypes.string.isRequired,
-    id: PropTypes.string.isRequired,
-    onReset: PropTypes.func.isRequired
-}
+import PlayerPreview from './PlayerPreview.jsx';
 
 class PlayerInput extends React.Component {
     constructor(props) {
