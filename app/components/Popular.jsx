738d10c0417aa98e20b23d0ed3c4a84e38fd0495
= <brent@dev.localdomain>
2017-07-20 12:21:44 -0500

    changed interval

--- app/components/Popular.jsx
+++ app/components/Popular.jsx
@@ -97,7 +97,7 @@ class Popular extends React.Component {
                     onSelect={this.updateLanguage}
                 />
                 {!this.state.repos
-                    ? <Loading />
+                    ? <Loading text='Downloading' speed={10} />
                     : <RepoGrid repos={this.state.repos} /> }                
             </div>
         )
